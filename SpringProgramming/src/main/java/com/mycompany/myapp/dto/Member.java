c66ac28a3f894a757e155cb7dc6e3761ddd6454d
egg_kim <yellcat@naver.com>
2015-09-04 15:14:32 +0900

    Member Dao, Dto, Service

---
+++ SpringProgramming/src/main/java/com/mycompany/myapp/dto/Member.java
@@ -0,0 +1,40 @@
+package com.mycompany.myapp.dto;
+
+public class Member {
+	private String id;
+	private String pw;
+	private String name;
+	private String mobile;
+	private String address;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
