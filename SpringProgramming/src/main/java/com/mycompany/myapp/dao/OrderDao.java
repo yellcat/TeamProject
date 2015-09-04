b8559b52021369c409c4c9a7d9bd67ff03edd46c
dami815 <dmkim815@gmail.com>
2015-09-04 15:18:44 +0900

    Order, OrderDao 완성

--- SpringProgramming/src/main/java/com/mycompany/myapp/dao/OrderDao.java
+++ SpringProgramming/src/main/java/com/mycompany/myapp/dao/OrderDao.java
@@ -1,7 +1,91 @@
 package com.mycompany.myapp.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import com.mycompany.myapp.dto.Order;
+
+@Component
 public class OrderDao {
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	   public Integer insert(Order order) {
+		      Integer pk = null;
+		      String sql = "insert into orders(order_date, order_payment, order_price, member_id) values(now(), ?, ?, ?)";
+		      KeyHolder keyHolder = new GeneratedKeyHolder();
+		      jdbcTemplate.update(new PreparedStatementCreator() {
+
+				@Override
+				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+					PreparedStatement pstmt = conn.prepareStatement(sql, new String[]{"order_no"});
+				      pstmt.setString(1, order.getPayment());
+				      pstmt.setInt(2, order.getPrice());
+				      pstmt.setString(3, order.getId());
+					return pstmt;
+				}
+		    	  
+		      }, keyHolder);
+		      Number keyNumber = keyHolder.getKey();
+		      pk = keyNumber.intValue();
+		      return pk;
+		   }
+		   
+		   public Order selectByPk(int orderNo) {		     
+		      String sql = "select*from orders where order_no=?";
+		      Order order = jdbcTemplate.queryForObject(
+		    		  sql, 
+		    		  new Object[] {orderNo}, 
+		    		  new RowMapper<Order>(){
+
+						@Override
+						public Order mapRow(ResultSet rs, int rowNo) throws SQLException {
+							Order order = new Order();
+							
+							order.setNo(rs.getInt("order_no"));
+					        order.setDate(rs.getDate("order_date"));
+					        order.setPayment(rs.getString("order_payment"));
+					        order.setPrice(rs.getInt("order_price"));
+					        order.setId(rs.getString("member_id"));
+					        
+							return order;
+						}
+		    		 }
+		    	);
+		      return order;
+		   }
+		   
+			public List <Order> selectById (String member_id) {
+			     String sql = "select * from orders where member_id = ?";
+			          
+			     List<Order> list = jdbcTemplate.query(
+			    		 sql,
+			    		 new Object[] {member_id},
+			    		 new RowMapper<Order>() {
+
+							@Override
+							public Order mapRow(ResultSet rs, int arg1) throws SQLException {
+								Order order = new Order();
+								order.setNo(rs.getInt("order_no"));
+								order.setId(rs.getString("member_id"));
+								order.setPrice(rs.getInt("order_price"));
+								return order;
+							}			    			 
+			    		 }			    		 
+			    	);
+			     	return list;
+			    }
 }
+			    		 
+			   
