00e7aca13427a23a7a2030b56f8ed97f448c5d54
dami815 <dmkim815@gmail.com>
2015-09-04 17:03:18 +0900

    CartDao, Cart, CartService 생성

---
+++ SpringProgramming/src/main/java/com/mycompany/myapp/dto/Cart.java
@@ -0,0 +1,34 @@
+package com.mycompany.myapp.dto;
+
+public class Cart {
+
+	private String memberId;
+	private int productNo;
+	private int cartAmount;
+	private int cartNo;
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	public int getCartAmount() {
+		return cartAmount;
+	}
+	public void setCartAmount(int cartAmount) {
+		this.cartAmount = cartAmount;
+	}
+	public int getCartNo() {
+		return cartNo;
+	}
+	public void setCartNo(int cartNo) {
+		this.cartNo = cartNo;
+	}
+}
